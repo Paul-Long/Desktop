a747d373f9a2861fb6364f561ebabc09b17cfe76
paul <7328194@163.com>
2018-05-15 10:20:47 +0800

    update

--- src/entries/index.js
+++ src/entries/index.js
@@ -3,15 +3,18 @@ import router from '@routes';
 import Helper from 'react-redux-helper';
 import models from '@models';
 
-const app = new Helper();
-app.router(router);
-app.reducerMiddleware(function (state, action) {
+const onReducer = (state, action) => {
   if (typeof state === 'object') {
     return { ...state };
   } else if (state instanceof Array) {
     return [...state];
   }
   return state;
+};
+
+const app = new Helper({
+  onReducer
 });
+app.router(router);
 app.models(models);
 app.start('#main');
--- src/entries/login.js
+++ src/entries/login.js
@@ -3,7 +3,20 @@ import router from '@routes/login';
 import Helper from 'react-redux-helper';
 import user from '@models/user';
 
-const login = new Helper();
+const onEffect = (...arg) => {
+  console.log(arg);
+  return function* (...arg) {
+    try {
+      console.log(arg);
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+};
+
+const login = new Helper({
+  onEffect
+});
 login.router(router);
 login.model(user);
 login.start('#main');
